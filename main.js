9f69fa87b3cdef7599803ef6103834387fb246b8
David Mwaura <ddmwaura@outlook.com>
2026-08-05 12:54:08 -0400

    Added texture and torus

--- main.js
+++ main.js
@@ -9,10 +9,17 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
+// Creates what the cube should look like
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const texture = new THREE.TextureLoader().load('SITKOL.jpg')
+const material = new THREE.MeshBasicMaterial({map : texture});
 const cube = new THREE.Mesh( geometry, material );
 scene.add(cube);
+
+const torus_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+const torus_tex = new THREE.MeshBasicMaterial({color : 0xe8f5ff});
+const torus = new THREE.Mesh(torus_geo, torus_tex);
+scene.add(torus);
 
 camera.position.z = 5;
 
@@ -20,6 +27,10 @@ function animate()
 {
     // Update objects
     cube.rotation.y += 0.01
+    cube.rotation.z += 0.01
+
+    torus.rotation.y += 0.01
+    torus.rotation.x += 0.01
 
     // Render
     renderer.render(scene, camera)
